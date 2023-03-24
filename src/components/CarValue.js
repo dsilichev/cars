84366b52ab63e9e3c102b87f63f10f6fdf6373c4
Dmitry <dsilichev@gmail.com>
2023-03-24 14:41:17 +0300

    total cost refactor

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,17 +1,13 @@
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
 
 function CarValue() {
-  const totalCost = useSelector(({cars: { data, searchTerm }}) => {
-    const filteredCars = data.filter((car) => {
-      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    let cost = 0;
-    for (let car of filteredCars) {
-      cost += car.cost;
-    }
-    return cost;
-  });
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) =>
+    data
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, car) => acc + car.cost, 0)
+  );
 
   return <div className="car-value">Total cost: ${totalCost}</div>;
 }
